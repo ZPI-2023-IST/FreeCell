484590a0b50ea68bca8be97a0b12f77b46ea4416
Mateusz <mateusz.wozniak.0211@gmail.com>
2023-11-08 14:47:42 +0100

    Z2-44 Added a test containing 20 move sequence

--- tests/test_Freecell.py
+++ tests/test_Freecell.py
@@ -90,6 +90,46 @@ class TestFreecell(TestCase):
 
         assert freecell._move_count == 4
 
+    def test_scenario_20_moves(self):
+        freecell = self.setup_scenario_various_moves()
+
+        assert freecell.make_move(('Ah', 'S'))
+        assert freecell.make_move(('9s', 'F'))
+        assert freecell.make_move(('9d', 'F'))
+        assert freecell.make_move(('Ad', 'S'))
+        assert freecell.make_move(('5s', 'F'))
+        assert freecell.make_move(('Js', 'F'))
+        assert freecell.make_move(('9d', 'Ts'))
+        assert freecell.make_move(('6c', 'F'))
+        assert not freecell.make_move(('Ks', 'F'))
+        assert freecell.make_move(('5h', '6s'))
+        assert freecell.make_move(('9s', 'Td'))
+        assert freecell.make_move(('4c', '5h'))
+        assert not freecell.make_move(('Kc', 'S'))
+        assert freecell.get_state() == State.ONGOING
+        assert freecell.make_move(('7s', 'F'))
+        assert freecell.make_move(('6c', '7d'))
+        assert freecell.make_move(('9s', 'F'))
+        assert freecell.make_move(('Td', 'Jc'))
+        assert freecell.make_move(('9s', 'Td'))
+        assert freecell.make_move(('7s', '8d'))
+        assert freecell.make_move(('Kd', 'F'))
+        assert freecell.make_move(('9c', 'F'))
+        assert freecell.make_move(('Js', 'Qh'))
+        assert freecell.make_move(('7s', 'F'))
+        assert freecell.make_move(('8d', '9s'))
+        assert freecell.make_move(('7s', '8d'))
+        assert not freecell.make_move(('9s', 'Td'))
+        assert freecell.make_move(('5c', 'F'))
+        assert freecell.make_move(('Ac', 'S'))
+        assert freecell.make_move(('Th', 'Js'))
+        assert freecell.make_move(('9c', 'Th'))
+        assert freecell.make_move(('5c', '0'))
+        assert freecell.make_move(('7c', 'F'))
+        assert freecell.make_move(('4d', 'F'))
+        assert freecell.make_move(('4h', '5c'))
+        assert freecell.get_state() == State.ONGOING
+
     def test_scenario_empty_board(self):
         freecell = self.setup_scenario_empty_board()
 
