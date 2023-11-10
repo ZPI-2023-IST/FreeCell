df2db037c2d094d35f69bfb648d654d7194dc924
Kamil Krawiec <95848075+Kamil-Krawiec@users.noreply.github.com>
2023-11-10 16:56:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="freecell",
-    version="0.0.3",
+    version="0.0.4",
     author="Mateusz and Kamil",
     description="A Freecell game",
     packages=setuptools.find_packages(),
